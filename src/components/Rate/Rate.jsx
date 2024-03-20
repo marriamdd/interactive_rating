1ff16f17412819ddfd85d0bd029bc9f68b3b15f5
marriamdd <davitashvili.m3@gmail.com>
2024-03-21 02:30:32 +0400

    add submit button

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,6 +1,8 @@
-import './rate.css'
+import './rate.css';
+
 export default function Rate() {
-    let buttons=[1,2,3,4,5]
+  let buttons = [1, 2, 3, 4, 5];
+
   return (
     <div className="rate_container">
       <h1>How did we do?</h1>
@@ -8,7 +10,13 @@ export default function Rate() {
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
-      {buttons.map((btn)=>(<buttons>{btn}</buttons>))}
+      <div className='btns_container'> 
+      {buttons.map((btn,index) => (
+        <button key={index} className="rate">{btn}</button>
+
+      ))}
+      </div>
+      <button className='submit'>Submit</button>
     </div>
   );
 }
